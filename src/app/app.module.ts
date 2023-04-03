1347e0161d16bbbffb9087b36044c0e7bc596293
Jason <hieutrantvvn2006@gmail.com>
2023-04-02 23:59:26 -0400

    add container and nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    ContainerComponent,
+      NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
